0bb0259b8b9a64f52ec89be07ae8af89e5c147cd
Jaylaelike <smarkwisai@gmail.com>
2024-11-28 11:10:56 +0700

    first commit

--- drizzle/0000_oval_the_watchers.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `users` (
-	`id` text PRIMARY KEY NOT NULL,
-	`email` text NOT NULL,
-	`name` text,
-	`role` text DEFAULT 'user' NOT NULL,
-	`random_number` integer,
-	`created_at` integer
-);
